2c60a87fec776c9381f36f0eb69485247976b035
kjx63 <ueyama.kenji.63@gmail.com>
2020-08-29 21:23:31 +0900

    Build Recommended Videos components

--- src/Components/RecommendedVideos.js
+++ src/Components/RecommendedVideos.js
@@ -1,10 +1,77 @@
 import React from 'react';
 import './RecommendedVideos.css';
+import VideoCard from './VideoCard';
 
 const RecommendedVideos = () => {
   return (
     <div className='recommendedVideos'>
       <h2>Recommended</h2>
+      <div className='recommendedVideos__videos'>
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='875 views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/a/AATXAJy5xCV9uU9I5fWNih0v43nwyved5bm9x2a-o8j95oc=s176-c-k-c0x00ffffff-no-rj-mo'
+          channel='Sonny Sangha'
+          image='https://i.ytimg.com/an_webp/UaB_FtsdlYk/mqdefault_6s.webp?du=3000&sqp=CLmEqfoF&rs=AOn4CLDJWHOtwtUv6ezh0_ecnYPJvHnVNw'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='875 views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/a/AATXAJy5xCV9uU9I5fWNih0v43nwyved5bm9x2a-o8j95oc=s176-c-k-c0x00ffffff-no-rj-mo'
+          channel='Sonny Sangha'
+          image='https://i.ytimg.com/an_webp/UaB_FtsdlYk/mqdefault_6s.webp?du=3000&sqp=CLmEqfoF&rs=AOn4CLDJWHOtwtUv6ezh0_ecnYPJvHnVNw'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='875 views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/a/AATXAJy5xCV9uU9I5fWNih0v43nwyved5bm9x2a-o8j95oc=s176-c-k-c0x00ffffff-no-rj-mo'
+          channel='Sonny Sangha'
+          image='https://i.ytimg.com/an_webp/UaB_FtsdlYk/mqdefault_6s.webp?du=3000&sqp=CLmEqfoF&rs=AOn4CLDJWHOtwtUv6ezh0_ecnYPJvHnVNw'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='875 views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/a/AATXAJy5xCV9uU9I5fWNih0v43nwyved5bm9x2a-o8j95oc=s176-c-k-c0x00ffffff-no-rj-mo'
+          channel='Sonny Sangha'
+          image='https://i.ytimg.com/an_webp/UaB_FtsdlYk/mqdefault_6s.webp?du=3000&sqp=CLmEqfoF&rs=AOn4CLDJWHOtwtUv6ezh0_ecnYPJvHnVNw'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='875 views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/a/AATXAJy5xCV9uU9I5fWNih0v43nwyved5bm9x2a-o8j95oc=s176-c-k-c0x00ffffff-no-rj-mo'
+          channel='Sonny Sangha'
+          image='https://i.ytimg.com/an_webp/UaB_FtsdlYk/mqdefault_6s.webp?du=3000&sqp=CLmEqfoF&rs=AOn4CLDJWHOtwtUv6ezh0_ecnYPJvHnVNw'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='875 views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/a/AATXAJy5xCV9uU9I5fWNih0v43nwyved5bm9x2a-o8j95oc=s176-c-k-c0x00ffffff-no-rj-mo'
+          channel='Sonny Sangha'
+          image='https://i.ytimg.com/an_webp/UaB_FtsdlYk/mqdefault_6s.webp?du=3000&sqp=CLmEqfoF&rs=AOn4CLDJWHOtwtUv6ezh0_ecnYPJvHnVNw'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='875 views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/a/AATXAJy5xCV9uU9I5fWNih0v43nwyved5bm9x2a-o8j95oc=s176-c-k-c0x00ffffff-no-rj-mo'
+          channel='Sonny Sangha'
+          image='https://i.ytimg.com/an_webp/UaB_FtsdlYk/mqdefault_6s.webp?du=3000&sqp=CLmEqfoF&rs=AOn4CLDJWHOtwtUv6ezh0_ecnYPJvHnVNw'
+        />
+        <VideoCard
+          title='Become a Web Developer in 10 minutes | 2019/2020'
+          views='875 views'
+          timestamp='3 days ago'
+          channelImage='https://yt3.ggpht.com/a/AATXAJy5xCV9uU9I5fWNih0v43nwyved5bm9x2a-o8j95oc=s176-c-k-c0x00ffffff-no-rj-mo'
+          channel='Sonny Sangha'
+          image='https://i.ytimg.com/an_webp/UaB_FtsdlYk/mqdefault_6s.webp?du=3000&sqp=CLmEqfoF&rs=AOn4CLDJWHOtwtUv6ezh0_ecnYPJvHnVNw'
+        />
+      </div>
     </div>
   );
 };
